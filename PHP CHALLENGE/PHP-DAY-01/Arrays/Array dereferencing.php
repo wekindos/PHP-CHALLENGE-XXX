0b8a022dba4ac83abb8c1acd1f7233c24fe0e164
ROGASIAN MVUNGI <102894678+wekindos@users.noreply.github.com>
2023-03-02 16:21:27 -0800

    Delete PHP CHALLENGE/PHP-DAY-01 directory

--- PHP CHALLENGE/PHP-DAY-01/Arrays/Array dereferencing.php
+++
@@ -1,9 +0,0 @@
-<?php 
-function getArray(){
-    return array(1,2,3);
-}
-$secondElement = getArray()[1];
-?>
-<!-- Note:
-
-Attempting to access an array key which has not been defined is the same as accessing any other undefined variable: an E_WARNING-level error message (E_NOTICE-level prior to PHP 8.0.0) will be issued, and the result will be null. -->
